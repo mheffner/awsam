dbd4f96bbecf8eb37749737ecd4811de9dd0ed61
John Claus <johnclaus@gmail.com>
2014-04-06 18:08:33 -0600

    Added the ability to assh into a node using a tag name; in dire need of refactoring

--- lib/awsam/ec2.rb
+++ lib/awsam/ec2.rb
@@ -20,7 +20,23 @@ module Awsam
         insts.each do |inst|
           return inst if inst[:aws_instance_id] == instance_id
         end
+      else
+        tags = ec2.describe_tags
+        if !tags || tags.length == 0
+          puts "No tags available in account"
+          return nil
+        end
+
+        tags.each do |tag|
+          if tag[:value] == instance_id
+            insts = ec2.describe_instances
+            insts.each do |inst|
+              return inst if inst[:aws_instance_id] == tag[:resource_id]
+            end
+          end
+        end
       end
+      
       return nil
     end
   end
